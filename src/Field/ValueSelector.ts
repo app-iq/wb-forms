b65a85afe2cf7e68e4f527c523f84d1c0ad55b90
Ali Faris <alifaris@earthlink.iq>
2021-11-05 22:21:49 +0300

    files selectors

--- src/Field/ValueSelector.ts
+++ src/Field/ValueSelector.ts
@@ -1,7 +1,14 @@
 import {FieldState} from "../Data/Types/FieldState";
 
-export type ValueSelector = (e: any , field : FieldState) => any;
+export type ValueSelector = (e: any, field: FieldState) => any;
 
 export const textValueSelector: ValueSelector = (e: any) => e.target.value;
 
-export const checkboxValueSelector : ValueSelector = (e: any) => e.target.checked;
+export const checkboxValueSelector: ValueSelector = (e: any) => e.target.checked;
+
+export const filesValueSelector: ValueSelector = (e: any) => e.target.files;
+
+export const singleFileSelector: ValueSelector = (e: any, fieldState: FieldState) => {
+    const files = filesValueSelector(e, fieldState);
+    return files && files.length > 0 ? files[0] : undefined;
+};
