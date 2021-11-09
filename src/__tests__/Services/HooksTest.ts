143bf0d012cc968dd9eb480204d3db2f7dbfffa8
Ali Faris <alifaris@earthlink.iq>
2021-11-09 22:12:05 +0300

    testing service factory hooks

---
+++ src/__tests__/Services/HooksTest.ts
@@ -0,0 +1,26 @@
+import {useServiceFactory} from "../../Services/ServiceFactory/Hooks";
+import React from "react";
+
+describe('ServiceFactoryHooks', () => {
+
+    jest.mock('react', () => ({
+        ...jest.requireActual('react'),
+        useContext: jest.fn()
+    }));
+
+    it('should throw exception when service factory is nullable', function () {
+        jest.spyOn(React, "useContext")
+            .mockReturnValueOnce(undefined);
+        expect(() => useServiceFactory()).toThrowError("cannot resolve service factory");
+    });
+
+    it('should return service factory', function () {
+        let mockedServiceFactory = {};
+        jest.spyOn(React, "useContext")
+            .mockReturnValueOnce(mockedServiceFactory);
+        let serviceFactory = useServiceFactory();
+        expect(serviceFactory).toEqual(mockedServiceFactory);
+    });
+
+
+})
