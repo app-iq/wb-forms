870b6291c6203e6311eff77f47b5fe0399931fac
Ali Faris <alihcompiler@gmail.com>
2022-09-24 20:46:52 +0300

    minor imporvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {SimpleExample} from './Examples/ExamplesPage';
+import {SimpleExample} from './__examples__/ExamplesPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
